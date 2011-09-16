dc525d1b2719a6855c6017fd3fb47062449edf27
Chris LaRose <cjlarose@gmail.com>
2011-09-15 21:50:23 -0700

    Integrated wizonesolutions's Meetup API.  Added ability to change group on options page

--- options-page.php
+++ options-page.php
@@ -5,12 +5,26 @@
 ?>
 <h2>WP Meetup Options</h2>
 <p class="description">
-    Options for Meetup.com integration
+    Options for Meetup.com integration by <a href="http://nuancedmedia.com/">Nuanced Media</a>
 </p>
 
 <?php if ($has_api_key): ?>
 
-<h3>I have an API KEY</h3>
+
+<pre>
+<?php
+        $this->mu_api->setQuery( array('group_urlname' => $this->options['group_url_name']) ); //Replace with a real group's URL name - it's what comes after the www.meetup.com/
+        set_time_limit(0);
+        $this->mu_api->setPageSize(200);
+        $response = $this->mu_api->getResponse();
+        //var_dump($response);
+?>
+</pre>
+
+<h3><?php echo $response->results[0]->name; ?></h3>
+<p>
+    <?php echo $response->results[0]->description; ?>
+</p>
 
 <?php endif; ?>
 
@@ -21,12 +35,24 @@
 <form id="wp-meetup-api-key" action="<?php echo $this->admin_page_url; ?>" method="post">
 <p>
     <label>Meetup.com API Key: </label>
-    <input type="text" name="api_key" value="<?php echo $this->api_key; ?>" />
+    <input type="text" name="api_key" value="<?php echo $this->options['api_key']; ?>" />
 </p>
 <p>
     <input type="submit" value="Submit" />
 </p>
 </form>
 
-
+<h3>Group Information</h3>
+<p>
+    To pull in your Meetup.com events, provide your group's Meetup.com URL, e.g. "http://www.meetup.com/tucsonhiking"
+</p>
+<form id="wp-meetup-api-key" action="<?php echo $this->admin_page_url; ?>" method="post">
+<p>
+    <label>Meetup.com Group URL: </label>
+    <input type="text" name="group_url" value="<?php echo $group_url; ?>" />
+</p>
+<p>
+    <input type="submit" value="Submit" />
+</p>
+</form>
 
--- wp-meetup.php
+++ wp-meetup.php
@@ -15,15 +15,25 @@ $meetup = new WP_Meetup();
 class WP_Meetup {
     
     private $dir;
-    private $api_key;
+    private $options;
     private $admin_page_url;
     private $mu_api;
     
     function WP_Meetup() {
         
+        if (!empty($_POST)) $this->handle_post_data();
+        
         $this->dir = WP_PLUGIN_DIR . "/wp-meetup/";
-        $this->api_key = get_option('wp_meetup_api_key') ? get_option('wp_meetup_api_key') : FALSE;
+        $this->options = array();
+        $this->options['api_key'] = get_option('wp_meetup_api_key') ? get_option('wp_meetup_api_key') : FALSE;
+        $this->options['group_url_name'] = get_option('wp_meetup_group_url_name') ? get_option('wp_meetup_group_url_name') : FALSE;
         $this->admin_page_url = admin_url("options-general.php?page=wp_meetup");
+        
+        //var_dump($this->options);
+        
+        include("meetup_api/MeetupAPIBase.php");
+        $this->mu_api = new MeetupAPIBase($this->options['api_key'], 'groups');
+        
         
         add_action('admin_menu', array($this, 'admin_menu'));
         
@@ -33,20 +43,29 @@ class WP_Meetup {
         add_options_page('WP Meetup Options', 'WP Meetup', 'manage_options', 'wp_meetup', array($this, 'admin_options'));
     }
     
+    function handle_post_data() {
+        if (array_key_exists('api_key', $_POST)) {
+            update_option('wp_meetup_api_key', $_POST['api_key']);
+            $this->options['api_key'] = $_POST['api_key'];
+        }
+        
+        
+        if (array_key_exists('group_url', $_POST)) {
+            $parsed_name = str_replace("http://www.meetup.com/", "", $_POST['group_url']);
+            $parsed_name = substr($parsed_name, 0, strpos($parsed_name, "/"));
+            update_option('wp_meetup_group_url_name', $parsed_name);
+            $this->options['group_url_name'] = $parsed_name;
+        }
+    }
+    
     function admin_options() {
         if (!current_user_can('manage_options'))  {
 		wp_die( __('You do not have sufficient permissions to access this page.') );
 	}
         
-        if (!empty($_POST)){
-            if (array_key_exists('api_key', $_POST)) {
-                update_option('wp_meetup_api_key', $_POST['api_key']);
-            }
-            $this->api_key = get_option('wp_meetup_api_key') ? get_option('wp_meetup_api_key') : FALSE;
-        }
-        
         $data = array();
-        $data['has_api_key'] = !empty($this->api_key);
+        $data['has_api_key'] = !empty($this->options['api_key']);
+        $data['group_url'] = !empty($this->options['group_url_name']) ? "http://www.meetup.com/" . $this->options['group_url_name'] : FALSE;
         
         echo $this->get_include_contents($this->dir . "options-page.php", $data);
         
