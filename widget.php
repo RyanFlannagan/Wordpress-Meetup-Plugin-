adf594f51b3ed458d5f3ee73796f305a042a19fe
Chris LaRose <cjlarose@gmail.com>
2011-09-19 16:02:10 -0700

    Changed name of widget on widget page

--- widget.php
+++ widget.php
@@ -2,7 +2,7 @@
 class WP_Meetup_Calendar_Widget extends WP_Widget {
     
     function WP_Meetup_Calendar_Widget() {
-        parent::WP_Widget( 'wp_meetup_calendar_widget', $name = 'WP_Meetup_Calendar_Widget' );
+        parent::WP_Widget( 'wp_meetup_calendar_widget', $name = 'WP Meetup Calendar Widget' );
     }
     
     function form($instance) {
