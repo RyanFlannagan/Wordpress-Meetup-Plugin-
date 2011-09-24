22b5154ad47e678d39b692d3068cea3bf632b9a7
Chris LaRose <cjlarose@gmail.com>
2011-09-24 15:31:24 -0700

    Added GPL2 License comment

--- wp-meetup.php
+++ wp-meetup.php
@@ -6,6 +6,21 @@ Description: Pulls events from Meetup.com onto your blog
 Version: 1.0
 Author: Nuanced Media
 Author URI: http://nuancedmedia.com/
+
+Copyright 2011  Nuanced Media
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 include(dirname(__FILE__) . DIRECTORY_SEPARATOR . "meetup_api/MeetupAPIBase.php");
